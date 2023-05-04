41a521f7c345ce8c5eb25e51f82473a279b09ef8
Oscar <Osm1019@gmail.com>
2023-05-03 17:27:50 -0700

    remove gs201:optimize scrolling

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -449,8 +449,6 @@ BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 PRODUCT_COPY_FILES += \
     device/google/gs201/conf/adaptivecharging.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/adaptivecharging.xml
     
-# Properties
-TARGET_VENDOR_PROP += device/google/gs201/vendor.prop
 
 
 
